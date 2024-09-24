bd034e120622dd857bdb1ee8d33fb9841df05e76
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2024-09-24 22:42:16 +0100

    Add project files.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Snowman% mod for I Am Your Beast
+
+This mod:
+
+- Adds a popup that appears telling you your snowman% time after you brutally murder an innocent snowman
+- you monster
+
+# Installation
+
+!!! You need [Bepinex 6](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) for mono !!!
+(if you have no idea what the versions mean try BepInEx-Unity.Mono-win-x64-6.0.0-pre.2 and it might work. maybe)
+
+Once bepinex is installed, put the dll into the BepInEx/plugins folder in the game's root directory.
+
+have fun :3
+
+![](https://files.catbox.moe/sixk00.jpg)
---
+++ Snowman%/Mod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace Snowman_
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginversion)]
+    public class Mod : BaseUnityPlugin
+    {
+        public const string pluginGuid = "kestrel.iamyourbeast.snowmanpercent";
+        public const string pluginName = "Snowman%";
+        public const string pluginversion = "1.1.0";
+
+        public void Awake() {
+
+            Logger.LogInfo("Hiiiiiiiiiiii :3");
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Snowman), "Kill")]
+    public class PatchSnowman
+    {
+        [HarmonyPostfix]
+        public static async void Postfix() {
+            float time = GameManager.instance.levelController.GetCombatTimer().GetTime();
+            GameManager.instance.player.GetHUD().GetNotificationPopUp().TriggerPopUp($"Snowman%: {time:0.00}", HUDNotificationPopUp.ThreatLevel.High);
+        }
+    }
+    
+
+}
